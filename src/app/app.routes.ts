6d94475e2b3cfafd2a00c59711bda2d6910a5e58
Victor Ross <vicjun22@gmail.com>
2025-03-28 10:29:58 -0300

    Change SignUp component to main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: CadastroComponent,
-        title: 'Avenida Gás Braz'
-    },
-    {
-        path: 'teste/cadastro/cliente',
         component: SignUpComponent,
         title: 'Avenida Gás Braz'
     }
